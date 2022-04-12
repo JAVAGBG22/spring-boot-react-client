a12209ca46c5382f70b3f7006f8443516405b410
Helena Johansson <helena@klarr.se>
2022-04-13 01:52:44 +0200

    simple todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Header from "./Header";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    if (inputValue.trim() === "") return;
+
+    setTodos([
+      ...todos,
+      {
+        text: inputValue,
+        id: uuidv4(),
+      },
+    ]);
+
+    setInputValue("");
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <form onSubmit={addTodo}>
+          <input
+            autoFocus
+            type="text"
+            placeholder="Add a ToDo"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button className="btn">Add ToDo</button>
+        </form>
+        {todos.map((todo) => (
+          <div key={todo.id} className="todo">
+            <p>{todo.text}</p>
+            <button className="btn-delete" onClick={() => removeTodo(todo.id)}>
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div
+          style={{
+            textDecoration: "none",
+            color: "#bfff00",
+          }}
+        >
+          <h2>Todo App created with Spring Boot, React and MongoDB</h2>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
